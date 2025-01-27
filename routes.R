cbeb65d12a565593b6c58558a7003372fc0af77e
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-01-26 21:34:04 -0500

    Add files via upload

--- routes.R
+++ routes.R
@@ -1,46 +1,44 @@
-#
-# routes
-# --------
+# Configuration File
+# -------------------
 
-APP_HOME= "C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade"
-CODE_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/codigo de aplicacion"
-MUESTRA_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/muestra cmd"
-REGISTROS_RUTA_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/raw participation data"
-PARTICIPATION_RECORDS_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/normalized participacion records"
+APP_HOME <- dirname(rstudioapi::getActiveDocumentContext()$path)
 
-#
-ACTIVITIES_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/activities"
-#
-ASSISTANCE_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/assistance"
-#
-EXPERIMENTS_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/experiments"
-#
-ASSESTMENT_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/assestment"
-#
-LABELLED_SAMPLE_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/labelled sample"
 
-OFERENTES_HOME= "C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/oferentes entradas"
-PEOPLE_ASSISTED="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/people assisted"
-EXPERIMENTAL_CASES_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/experimental cases"
-CONTRATACIONES_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/contrataciones"
-CONTRATACIONES2_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/oferentes colocados"
-DEMANDA_DE_TRABAJO_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/demanda de trabajo"
-COLOCACIONES_HOME = "C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/raw participation data" 
-PROFILES_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/perfiles virtuales"
-CONTRATACIONES_ESTANDARIZADAS_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/contrataciones estandarizadas"
-LABORAL_OUTCOMES_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/laboral outcomes"
-REGISTROS_VACANTES_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/vacantes disponibles"
+MUESTRA_HOME <- file.path(APP_HOME, "repositorios de datos/muestra cmd")
+REGISTROS_RUTA_HOME <- file.path(APP_HOME, "repositorios de datos/raw participation data")
+PARTICIPATION_RECORDS_HOME <- file.path(APP_HOME, "repositorios de datos/normalized participacion records")
 
-CONTROL_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/oferentes validacion"
-PROCESS_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/raw processes data"
-UNBALANCED_SAMPLE_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/unbalanced sample"
-HOJAS_DE_VIDA_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/raw hojas de vida"
-PERFIL_LABORAL_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/perfil laboral"
-MATRIZ_EVALUACION_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/matriz evaluacion"
-INFO_BASICA_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/info basica"
-INSIGHTS_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/insights on placements rates"
-COVID_DESIGN_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/muestra covid"
-PROCESOS_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/procesos"
-CANDIDATES_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/postulaciones"
-VACANTES_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/vacantes"
-TECNOLOGÍAS_HOME="C:/Users/josio/OneDrive - Fedesarrollo/proyecto grade/repositorios de datos/tecnologias"
+ACTIVITIES_HOME <- file.path(APP_HOME, "repositorios de datos/activities")
+ASSISTANCE_HOME <- file.path(APP_HOME, "repositorios de datos/assistance")
+EXPERIMENTS_HOME <- file.path(APP_HOME, "repositorios de datos/experiments")
+ASSESSMENT_HOME <- file.path(APP_HOME, "repositorios de datos/assessment")
+LABELLED_SAMPLE_HOME <- file.path(APP_HOME, "repositorios de datos/labelled sample")
+
+OFERENTES_HOME <- file.path(APP_HOME, "repositorios de datos/oferentes entradas")
+PEOPLE_ASSISTED <- file.path(APP_HOME, "repositorios de datos/people assisted")
+EXPERIMENTAL_CASES_HOME <- file.path(APP_HOME, "repositorios de datos/experimental cases")
+CONTRATACIONES_HOME <- file.path(APP_HOME, "repositorios de datos/contrataciones")
+CONTRATACIONES2_HOME <- file.path(APP_HOME, "repositorios de datos/oferentes colocados")
+DEMANDA_DE_TRABAJO_HOME <- file.path(APP_HOME, "repositorios de datos/demanda de trabajo")
+COLOCACIONES_HOME <- file.path(APP_HOME, "repositorios de datos/raw participation data")
+PROFILES_HOME <- file.path(APP_HOME, "repositorios de datos/perfiles virtuales")
+CONTRATACIONES_ESTANDARIZADAS_HOME <- file.path(APP_HOME, "repositorios de datos/contrataciones estandarizadas")
+LABORAL_OUTCOMES_HOME <- file.path(APP_HOME, "repositorios de datos/laboral outcomes")
+REGISTROS_VACANTES_HOME <- file.path(APP_HOME, "repositorios de datos/vacantes disponibles")
+
+CONTROL_HOME <- file.path(APP_HOME, "repositorios de datos/oferentes validacion")
+PROCESS_HOME <- file.path(APP_HOME, "repositorios de datos/raw processes data")
+UNBALANCED_SAMPLE_HOME <- file.path(APP_HOME, "repositorios de datos/unbalanced sample")
+HOJAS_DE_VIDA_HOME <- file.path(APP_HOME, "repositorios de datos/raw hojas de vida")
+PERFIL_LABORAL_HOME <- file.path(APP_HOME, "repositorios de datos/perfil laboral")
+MATRIZ_EVALUACION_HOME <- file.path(APP_HOME, "repositorios de datos/matriz evaluacion")
+INFO_BASICA_HOME <- file.path(APP_HOME, "repositorios de datos/info basica")
+INSIGHTS_HOME <- file.path(APP_HOME, "repositorios de datos/insights on placements rates")
+COVID_DESIGN_HOME <- file.path(APP_HOME, "repositorios de datos/muestra covid")
+PROCESOS_HOME <- file.path(APP_HOME, "repositorios de datos/procesos")
+CANDIDATES_HOME <- file.path(APP_HOME, "repositorios de datos/postulaciones")
+VACANTES_HOME <- file.path(APP_HOME, "repositorios de datos/vacantes")
+TECNOLOGIAS_HOME <- file.path(APP_HOME, "repositorios de datos/tecnologias")
+
+WORKERS_SAMPLE_HOME <-APP_HOME
+CODE_HOME <- APP_HOME
